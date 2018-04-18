db40da90fcdc5684fba29244b5b825c8d3c95f43
Eduardo Gomez <eduardo.www@gmail.com>
2018-04-17 19:37:16 -0500

    Cobro FrontEnd

---
+++ src/App.js
@@ -0,0 +1,186 @@
+import React, { Component, Fragment } from 'react';
+import logo from './logo.png';
+import methods_statement_cards from './methods_statement_cards.png';
+import oxxopay from './oxxopay.svg';
+import paypal from './paypal.svg';
+import './App.css';
+import Switch from 'react-router-dom/es/Switch';
+import Route from 'react-router-dom/es/Route';
+import Redirect from 'react-router-dom/es/Redirect';
+import BrowserRouter from 'react-router-dom/es/BrowserRouter';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/productos/:id" component={Item} />
+          <Route path="/" exact strict component={Item} />
+          <Redirect to="/" />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
+
+class Item extends Component {
+  styles = {
+    itemContainer: {
+      display: 'flex',
+      height: '50rem',
+      paddingTop: '5rem',
+      paddingLeft: '15rem',
+      paddingRight: '10rem'
+    },
+    navContainer: {
+      position: 'fixed',
+      padding: '1rem',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      height: '4rem',
+      width: '100%',
+      backgroundColor: 'white',
+      borderBottom: '1px solid rgba(0,0,0,.1)'
+    },
+    navLogo: {
+      height: '80%'
+    },
+    navLinksContainer: {
+      display: 'flex',
+      paddingRight: '4rem'
+    },
+    navLink: {
+      fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
+      fontWeight: '300',
+      fontSize: '1.1rem',
+      wordWrap: 'break-word',
+      textDecoration: 'none',
+      color: 'inherit',
+      marginRight: '1rem'
+    },
+    imageContainer: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      height: '100%',
+      flexGrow: 3
+    },
+    itemImage: {
+      height: '80%',
+      width: 'auto'
+    },
+    detailsContainer: {
+      paddingLeft: '6rem',
+      paddingTop: '4rem',
+      overflow: 'auto',
+      flexGrow: 7
+    },
+    title: {
+      fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
+      fontWeight: '300',
+      fontSize: '2rem',
+      wordWrap: 'break-word'
+    },
+    description: {
+      fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
+      fontWeight: '400',
+      fontSize: '1.1rem',
+      wordWrap: 'break-word'
+    },
+    details: {
+      fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
+      fontWeight: '300',
+      fontSize: '1.1rem',
+      wordWrap: 'break-word'
+    },
+    price: {
+      fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
+      fontSize: '1.2rem',
+      fontWeight: '400',
+      wordWrap: 'break-word'
+    },
+    shipping: {
+      fontFamily: "'Helvetica Neue',Helvetica,Arial,sans-serif",
+      fontSize: '1.3rem',
+      fontWeight: '500',
+      wordWrap: 'break-word'
+    }
+  };
+  products = [
+    {
+      id: 'producto1',
+      title: 'Digestion',
+      description:
+        'Producto: Digestión Pura. Colon limpio y sana digestión. No mas estreñimiento. Aporta mucílagos naturales, los cuales ayudan a  limpiar el colon de una manera eficáz y natural, sin dañar la salud.',
+      price: 10000.0,
+      image:
+        'http://www.suiiki.com/wp-content/uploads/2018/01/DigestionPura_small-1-300x155.png',
+      shipping: 'No shipping'
+    },
+    {
+      id: 'producto2',
+      title: 'Fitoestrogenos',
+      description:
+        ' No contiene sustancias que pongan en riesgo la salud de la mujer, ya que su elaboración es a base de Fito estrógenos obtenidos de semillas y plantas ricas en estrógenos femeninos naturales.',
+      price: 8000.0,
+      image:
+        'http://www.suiiki.com/wp-content/uploads/2018/01/Fitoestrogenos_small-1-300x200.png',
+      shipping: 'No shipping'
+    },
+    {
+      id: 'producto3',
+      title: 'Quema Grasa Corporal',
+      description:
+        'Ayudan a regular el apetito, causando sensación de saciedad. Es un excelente adelgazante y antinflamatorio. No causa efectos dañinos a la salud. No provoca rebote, ya que es natural y su efecto es gradual.',
+      price: 100.0,
+      image:
+        'http://www.suiiki.com/wp-content/uploads/2018/01/QuemaGrasa_big-300x200.png',
+      shipping: `Orders are printed and shipped when the time expires or earlier.
+              You can expect your package to arrive 10 - 13 business days after
+              the product prints. Expedited or Rush shipping may be available
+              depending upon the product(s) selected and the destination
+              country. Shipping costs start at: $3.99 for the first apparel item
+              and $2.00 for each additional apparel item. Products are fulfilled
+              in the US`
+    }
+  ];
+  render() {
+    const { styles, products } = this;
+    const product = products.find(p => p.id == this.props.match.params.id) || {
+      ...products[Math.floor(Math.random() * products.length)]
+    };
+    return (
+      <Fragment>
+        <div style={styles.navContainer}>
+          <img style={styles.navLogo} src={logo} alt="calithecat" />
+          <div style={styles.navLinksContainer}>
+            <a style={styles.navLink} href="http://midominio.com">
+              HOME
+            </a>
+            <a style={styles.navLink} href="http://midominio.com/blog">
+              BLOG
+            </a>
+            <a style={styles.navLink} href="http://midominio.com/contact">
+              CONTACT
+            </a>
+          </div>
+        </div>
+        <div style={styles.itemContainer}>
+          <div style={styles.imageContainer}>
+            <img style={styles.img} src={product.image} alt="Product Image" />
+          </div>
+          <div style={styles.detailsContainer}>
+            <h1 style={styles.title}>{product.title}</h1>
+            <p style={styles.description}>{product.description}</p>
+            <p style={styles.price}>${product.price} MXN</p>
+            <p style={styles.shipping}>Shipping Info</p>
+            <p style={styles.details}>{product.shipping}</p>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
